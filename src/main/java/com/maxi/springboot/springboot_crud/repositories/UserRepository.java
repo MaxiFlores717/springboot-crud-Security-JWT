5f34139e90aa9ad21749e585f3f8a750fa7a5ac6
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2025-12-17 20:52:50 -0300

    avance token

--- src/main/java/com/maxi/springboot/springboot_crud/repositories/UserRepository.java
+++ src/main/java/com/maxi/springboot/springboot_crud/repositories/UserRepository.java
@@ -1,11 +1,17 @@
 package com.maxi.springboot.springboot_crud.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.maxi.springboot.springboot_crud.entities.User;
+import java.util.List;
+
 
 public interface UserRepository extends CrudRepository<User, Long>{
 
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 
 }
